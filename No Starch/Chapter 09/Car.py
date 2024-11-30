9deb07e9d631bfa63850c284f0a8b514e8ae2b49
Steve <SteveCabral@hotmail.com>
2024-11-30 16:47:39 -0500

    Added some Python code

--- No Starch/Chapter 09/Car.py
+++ No Starch/Chapter 09/Car.py
@@ -1,12 +1,16 @@
+"""A class that can be used to represent a car."""
+
 class Battery:
 	"""A simple attempt to model a battery for an electric car."""
 	def __init__(self, battery_size=40):
 		"""Initialize the battery's attributes."""
 		self.battery_size = battery_size
 
+
 	def describe_battery(self):
 		"""Print a statement describing the battery size."""
 		print(f"This car has a {self.battery_size}-kWh battery.")
+
 
 	def get_range(self):
 		"""Print a statement about the range this battery provides."""
@@ -14,8 +18,8 @@ class Battery:
 			range = 150
 		elif self.battery_size == 65:
 			range = 225
-
-
+		
+		print(f"This car can go about {range} miles on a full charge.")
 
 
 class Car:
@@ -49,6 +53,10 @@ class Car:
 
 
 class ElectricCar(Car):
+	"""
+	Initialize attributes of the parent class.
+	Then initialize attributes specific to an electric car.
+	"""
 	def __init__ (self,make, model, year):
 		super().__init__(make, model, year)
 		self.battery = Battery()
@@ -58,28 +66,3 @@ class ElectricCar(Car):
 
 	def fill_gas_tank(self):
 		print("This is an electric vehicle... it does not have a gas tank.")
-
-
-my_new_car = Car('audi', 'a4', 2024)
-print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
-my_new_car.update_odometer(123)
-my_new_car.read_odometer()
-my_new_car.update_odometer(45)
-my_new_car.increment_odometer(23_500)
-my_new_car.read_odometer()
-my_new_car.fill_gas_tank(24)
-
-
-my_leaf = ElectricCar('nissan','leaf',2024)
-print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
-my_leaf.fill_gas_tank()
-my_leaf.battery.describe_battery()
-my_leaf.battery.get_range()
-
-
-
-
-my_new_car.read_odometer	# DO NOT CALL a class method like this without the parentheses since it will not throw an error but nothing happens
