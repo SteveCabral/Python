528bfeae2dc9e8498275814fd7c21b68b76749a9
Steve <SteveCabral@hotmail.com>
2026-09-18 12:05:57 -0400

    Removed PDF books that do not need storing in Git

--- Books/No Starch/Chapter 07/Chapter_07_Counting.py
+++
@@ -1,8 +0,0 @@
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number += 1
-
-current_number = 0
-print(f"current_number has been reset to {current_number}")
-
--- Books/No Starch/Chapter 08/Chapter_08_Modules.py
+++
@@ -1,8 +0,0 @@
-#FUNCTIONS:
-def VerifyUserList(list_copy):
-	for user in list_copy:
-		print(f"\nChecking user: {user}")
-		print(f"\t{user} has been verified.\n")
-	
-	return list_copy
-
